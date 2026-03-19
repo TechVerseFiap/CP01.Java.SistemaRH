defcf36d827501e482e8b46ddf5a7555930d52e9
Davi Munhoz <davimunhoz1005@gmail.com>
2026-03-18 22:33:26 -0300

    feat/sistema-ers-management: Creates exception for entity not found in list

---
+++ src/com/resources/human/domain/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.resources.human.domain.exceptions;
+
+public class EntityNotFoundException extends Exception {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
