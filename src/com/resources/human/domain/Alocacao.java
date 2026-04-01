f346ed3e90bc71f5e65663d3b7bb3cf8cb4e442e
Diogo Oliveira Lima <limaa.diogi@gmail.com>
2026-04-01 19:40:33 -0300

    alteracao

--- src/com/resources/human/domain/Alocacao.java
+++ src/com/resources/human/domain/Alocacao.java
@@ -52,8 +52,8 @@ public class Alocacao {
 
     public String exibir() {
         return "Alocacao{" +
-                "colaborador=" + colaborador +
-                ", recurso=" + recurso +
+                "colaborador=" + colaborador.exibir() +
+                ", recurso=" + recurso.exibir() +
                 ", data=" + data +
                 ", observacao='" + observacao + '\'' +
                 '}';
